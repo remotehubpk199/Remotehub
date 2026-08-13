19fa61d7ffb1eb22987099f1b2d88ef0e0ab971c
remotehubpk199 <remotehub.pk@gmail.com>
2026-08-13 19:15:24 +0500

    Create products.js

---
+++ products.js
@@ -0,0 +1,27 @@
+const products = [
+
+  {
+    name: "Gree YAN1F1 AC Remote",
+    category: "AC",
+    price: "Rs. 1,499",
+    image: "images/gree-yan1f1.jpg",
+    description: "Replacement remote for compatible Gree inverter AC models."
+  },
+
+  {
+    name: "TCL Smart LED Remote",
+    category: "TV",
+    price: "Rs. 1,299",
+    image: "images/tcl-smart.jpg",
+    description: "Smart LED TV replacement remote."
+  },
+
+  {
+    name: "Android Box Remote",
+    category: "Android",
+    price: "Rs. 999",
+    image: "images/android-box.jpg",
+    description: "Replacement remote for compatible Android TV boxes."
+  }
+
+];
